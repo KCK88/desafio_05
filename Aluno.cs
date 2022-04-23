c4da37c1ca9272dc2926cb022f4d8b7feca6a969
Claudio Meira <claudiomeirabs@gmail.com>
2022-04-22 22:33:46 -0300

    commit inicial

---
+++ Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace Desafio_05
+{
+    public class Aluno
+    {
+        public string Name { get; set; }
+        public int Idade { get; set; }
+        public double Nota { get; set; }
+    }
+
+}
